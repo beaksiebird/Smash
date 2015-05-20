da15fad55e6292171abc76d1d384f5f83657b4a0
Whitney Hogg <whitneylaurenhogg@gmail.com>
2015-05-20 12:10:10 -0400

    updates

--- Smash/Smash/GameData.swift
+++ Smash/Smash/GameData.swift
@@ -14,6 +14,12 @@ class GameData: NSObject {
     
     class func mainData() -> GameData {return _data }
     
+    var currentLevel = 0
+    var currentLives = 0
+    var currentScore = 0
+    
+    
+    
     var levels: [[[Int]]] = [
     
     [
@@ -32,4 +38,24 @@ class GameData: NSObject {
         ]
 
    ]
+    
+    var playerInfo : [String:AnyObject] = [
+    
+        "ballsLost" : 0,
+        "levelsBeat" : 0,
+        "topScore" : 0
+    
+    
+    
+    ]
+    
+    func getCurrentLevelBricks() -> [[Int]] {
+        
+        return levels[currentLevel]
+        
+        
+        
+    }
+    
+    
 }
