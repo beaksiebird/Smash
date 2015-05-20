2f3cf57dd5abf1704d7688f6923bfe2b2a7e75f0
Whitney Hogg <whitneylaurenhogg@gmail.com>
2015-05-20 09:11:40 -0400

    more stuff

--- Smash/Smash/AppDelegate.swift
+++ Smash/Smash/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  Smash
 //
-//  Created by Whitney Lauren on 5/19/15.
-//  Copyright (c) 2015 Whitney Lauren. All rights reserved.
+//  Created by Jo Albright on 5/19/15.
+//  Copyright (c) 2015 Jo Albright. All rights reserved.
 //
 
 import UIKit
